1596179e201d2c3f1e33b2de82ad5d19f796c11c
fetwi <fetwi.fitwi@gmail.com>
2024-08-09 10:47:43 -0400

    new file:   SM-2024-8-fr.html 	modified:   SMgen.py 	new file:   SMgen_old.py 	renamed:    smtemplate.html -> smtemplate_en.html 	new file:   smtemplate_fr.html 	renamed:    smurls.csv -> smurls_en.csv 	new file:   smurls_fr.csv 	new file:   version-html-complete-du-guide-des-approvisionnements.2023-2.html

--- SMgen.py
+++ SMgen.py
@@ -4,15 +4,18 @@ import os
 import csv
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-template_file = os.path.join(dir_path, "smtemplate.html")
-csv_file = os.path.join(dir_path, "smurls.csv")
 
+
+# def prompt_lang():
+#     return input('Enter the language of the SM (en or fr): ')
 # def prompt_version():
 #     return input('Enter the version (yyyy-m): ')
 # def prompt_edate():
 #     return input('Enter the effective date (yyyy-mm-dd): ')
+
 # def update_template():
 #     try:
+#         lang = prompt_lang()
 #         version = prompt_version()
 #         edate = prompt_edate()
 
@@ -20,116 +23,222 @@ def update_template():
     try:
         version = "2024-8"
         edate = "2024-08-06"
+        lang = "fr"
 
-        lang = "en"
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-        # Read the smtemplate.html file
-        with open(template_file, 'r', encoding='utf-8') as file:
-            template = file.read()
-        # Read the smurls.csv file
-        with open(csv_file, 'r', encoding='utf-8-sig') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                placeholder, url = row
-                response = requests.get(url, headers=headers)
-                response.raise_for_status()
-                # Parse the HTML content
-                soup = BeautifulSoup(response.content, 'html.parser')
-                chapter_content = soup.find('div', id='ChapterContent') or soup.find('div', id='smg-main')
-                if chapter_content is None:
-                    raise ValueError(f'ChapterContent div not found for URL: {url}')
-                # Remove all tags that are not <ul>, <li>, or <a>
 
-                if placeholder == '{toc}':
-                    # Remove the first <h3> tag
-                    first_h3 = chapter_content.find('h3')
-                    if first_h3:
-                        first_h3.decompose()
-                    last_a = chapter_content.find_all('a')[-1]
-                    if last_a:
-                        last_a.decompose()
-                    last_h3 = chapter_content.find_all('h3')[-1]
-                    if last_h3:
-                        a_gl_tag = soup.new_tag('a', href='#gl')
-                        a_gl_tag.string = '1 Supply Manual glossary'
-                        last_h3.replace_with(a_gl_tag)
-                    # Process remaining <h3> tags
-                    for h3 in chapter_content.find_all('h3'):
-                        strong_tag = h3.find('strong')
-                        if strong_tag and len(strong_tag.text) >= 5:
-                            href_second = strong_tag.text[9] if strong_tag.text[9].isdigit() else ''
-                            href_value = strong_tag.text[8]
-                            a_tag = soup.new_tag('a', href=f'#_{href_value}{href_second}')
-                            a_tag.string = f'{href_value}{href_second} {h3.text}'
-                            h3.replace_with(a_tag)
-                    for tag in chapter_content.find_all(True):
-                        if tag.name not in ['section', 'h3', 'strong', 'ul', 'li', 'a']:
-                            tag.decompose()
-                    # Replace all <section> tags with <li> tags
-                    for section in chapter_content.find_all('section'):
-                        li_tag = soup.new_tag('li')
-                        li_tag.extend(section.contents)
-                        section.replace_with(li_tag)
-                    # Modify href attributes in <a> tags within chapter_content
-                    for a in chapter_content.find_all('a', href=True):
-                        href = a['href']
-                        if '#' in href:
-                            # Remove everything before the #
-                            href = href[href.index('#'):]
-                            a['href'] = href
-                        if 'annex' in href:
-                            a_content = a.text
-                            an_chap = a_content[-2:] if len(a_content) == 22 else a_content[-1]
-                            href = f'#an_{an_chap}'
-                            a['href'] = href
+        if lang == 'en':
+            template_file = os.path.join(dir_path, "smtemplate_en.html")
+            csv_file = os.path.join(dir_path, "smurls_en.csv")            
+            # Read the smtemplate.html file
+            with open(template_file, 'r', encoding='utf-8') as file:
+                template = file.read()
+            # Read the smurls.csv file
+            with open(csv_file, 'r', encoding='utf-8-sig') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    placeholder, url = row
+                    response = requests.get(url, headers=headers)
+                    response.raise_for_status()
+                    # Parse the HTML content
+                    soup = BeautifulSoup(response.content, 'html.parser')
+                    chapter_content = soup.find('div', id='ChapterContent') or soup.find('div', id='smg-main')
+                    if chapter_content is None:
+                        raise ValueError(f'ChapterContent div not found for URL: {url}')
+                    if placeholder == '{toc}':
+                        # Remove the first <h3> tag
+                        first_h3 = chapter_content.find('h3')
+                        if first_h3:
+                            first_h3.decompose()
+                        last_a = chapter_content.find_all('a')[-1]
+                        if last_a:
+                            last_a.decompose()
+                        last_h3 = chapter_content.find_all('h3')[-1]
+                        if last_h3:
+                            a_gl_tag = soup.new_tag('a', href='#gl')
+                            a_gl_tag.string = '1 Supply Manual glossary'
+                            last_h3.replace_with(a_gl_tag)
+                        # Process remaining <h3> tags
+                        for h3 in chapter_content.find_all('h3'):
+                            strong_tag = h3.find('strong')
+                            if strong_tag and len(strong_tag.text) >= 5:
+                                href_second = strong_tag.text[9] if strong_tag.text[9].isdigit() else ''
+                                href_value = strong_tag.text[8]
+                                a_tag = soup.new_tag('a', href=f'#_{href_value}{href_second}')
+                                a_tag.string = f'{href_value}{href_second} {h3.text}'
+                                h3.replace_with(a_tag)
+                        for tag in chapter_content.find_all(True):
+                            if tag.name not in ['section', 'h3', 'strong', 'ul', 'li', 'a']:
+                                tag.decompose()
+                        # Replace all <section> tags with <li> tags
+                        for section in chapter_content.find_all('section'):
+                            li_tag = soup.new_tag('li')
+                            li_tag.extend(section.contents)
+                            section.replace_with(li_tag)
+                        # Modify href attributes in <a> tags within chapter_content
+                        for a in chapter_content.find_all('a', href=True):
+                            href = a['href']
+                            if '#' in href:
+                                # Remove everything before the #
+                                href = href[href.index('#'):]
+                                a['href'] = href
+                            if 'annex' in href:
+                                a_content = a.text
+                                an_chap = a_content[-2:] if len(a_content) == 22 else a_content[-1]
+                                href = f'#an_{an_chap}'
+                                a['href'] = href
+                    if 'an' in placeholder or 'ch' in placeholder:
+                        for tag in chapter_content.find_all(True):
+                            if tag.name in ['script']:
+                                tag.decompose()
+                    if 'ch' in placeholder:
+                        # Find the <a> tag with id "_Annexes" and modify its id
+                        annex_tag = chapter_content.find('a', id='_Annexes')
+                        if annex_tag:
+                            # Get the third and fourth characters of the placeholder
+                            third_char = placeholder[3]
+                            fourth_char = placeholder[4] if placeholder[4] != '}' else ''
+                            new_id = f'an_{third_char}{fourth_char}'
+                            annex_tag['id'] = new_id
+                        for a in chapter_content.find_all('a', href=True):
+                            href = a['href']
+                            if 'annex' in href:
+                                # Remove everything before the #
+                                href = href[href.index('#'):]
+                                href = '#an' + href[1:]
+                                a['href'] = href
+                    if 'an' in placeholder:
+                        for a in chapter_content.find_all('a', id=True):
+                            a_id = a['id']
+                            if '_' in a_id:
+                                # Remove everything before the #
+                                a_id = f'an{a_id}'
+                                a['id'] = a_id
 
-                if 'an' in placeholder or 'ch' in placeholder:
-                    for tag in chapter_content.find_all(True):
-                        if tag.name in ['script']:
-                            tag.decompose()
+                    chapter_content = chapter_content.decode_contents()
+                    toc = str(chapter_content)
 
-                if 'ch' in placeholder:
-                    # Find the <a> tag with id "_Annexes" and modify its id
-                    annex_tag = chapter_content.find('a', id='_Annexes')
-                    if annex_tag:
-                        # Get the third and fourth characters of the placeholder
-                        third_char = placeholder[3]
-                        fourth_char = placeholder[4] if placeholder[4] != '}' else ''
-                        new_id = f'an_{third_char}{fourth_char}'
-                        annex_tag['id'] = new_id
-                    for a in chapter_content.find_all('a', href=True):
-                        href = a['href']
-                        if 'annex' in href:
-                            # Remove everything before the #
-                            href = href[href.index('#'):]
-                            href = '#an' + href[1:]
-                            a['href'] = href
+                    # Replace the placeholder with the content
+                    template = template.replace(placeholder, toc)
 
-                if 'an' in placeholder:
-                    for a in chapter_content.find_all('a', id=True):
-                        a_id = a['id']
-                        if '_' in a_id:
-                            # Remove everything before the #
-                            a_id = f'an{a_id}'
-                            a['id'] = a_id
+            # Replace {version}, {edate}, and {lang} placeholders
+            template = template.replace('{version}', version)
+            template = template.replace('{edate}', edate)
+            template = template.replace('{lang}', lang)
 
-                chapter_content = chapter_content.decode_contents()
-                toc = str(chapter_content)
+            # Save the updated content to a new file
+            output_file_name = f'SM-{version}_{lang}.html'
+            with open(output_file_name, 'w', encoding='utf-8') as file:
+                file.write(template)
+            print(f'Template updated and saved as {output_file_name}')
 
-                # Replace the placeholder with the content
-                template = template.replace(placeholder, toc)
+        if lang == 'fr':
+            template_file = os.path.join(dir_path, "smtemplate_fr.html")
+            csv_file = os.path.join(dir_path, "smurls_fr.csv")
+            with open(template_file, 'r', encoding='utf-8') as file:
+                template = file.read()
+            # Read the smurls.csv file
+            with open(csv_file, 'r', encoding='utf-8-sig') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    placeholder, url = row
+                    response = requests.get(url, headers=headers)
+                    response.raise_for_status()
+                    # Parse the HTML content
+                    soup = BeautifulSoup(response.content, 'html.parser')
+                    chapter_content = soup.find('div', id='ChapterContent') or soup.find('div', id='smg-main')
+                    if chapter_content is None:
+                        raise ValueError(f'ChapterContent div not found for URL: {url}')
+                    if placeholder == '{toc}':
+                        # Remove the first <h3> tag
+                        first_h3 = chapter_content.find('h3')
+                        if first_h3:
+                            first_h3.decompose()
+                        last_a = chapter_content.find_all('a')[-1]
+                        if last_a:
+                            last_a.decompose()
+                        last_h3 = chapter_content.find_all('h3')[-1]
+                        if last_h3:
+                            a_gl_tag = soup.new_tag('a', href='#gl')
+                            a_gl_tag.string = '1 Glossaire du Guide des approvisionnements'
+                            last_h3.replace_with(a_gl_tag)
+                        # Process remaining <h3> tags
+                        for h3 in chapter_content.find_all('h3'):
+                            strong_tag = h3.find('strong')
+                            if strong_tag and len(strong_tag.text) >= 5:
+                                href_second = strong_tag.text[9] if strong_tag.text[9].isdigit() else ''
+                                href_value = strong_tag.text[8]
+                                a_tag = soup.new_tag('a', href=f'#_{href_value}{href_second}')
+                                a_tag.string = f'{href_value}{href_second} {h3.text}'
+                                h3.replace_with(a_tag)
+                        for tag in chapter_content.find_all(True):
+                            if tag.name not in ['section', 'h3', 'strong', 'ul', 'li', 'a']:
+                                tag.decompose()
+                        # Replace all <section> tags with <li> tags
+                        for section in chapter_content.find_all('section'):
+                            li_tag = soup.new_tag('li')
+                            li_tag.extend(section.contents)
+                            section.replace_with(li_tag)
+                        # Modify href attributes in <a> tags within chapter_content
+                        for a in chapter_content.find_all('a', href=True):
+                            href = a['href']
+                            if '#' in href:
+                                # Remove everything before the #
+                                href = href[href.index('#'):]
+                                a['href'] = href
+                            if 'annex' in href:
+                                a_content = a.text
+                                an_chap = a_content[-2:] if len(a_content) == 22 else a_content[-1]
+                                href = f'#an_{an_chap}'
+                                a['href'] = href
 
-        # Replace {version}, {edate}, and {lang} placeholders
-        template = template.replace('{version}', version)
-        template = template.replace('{edate}', edate)
-        template = template.replace('{lang}', lang)
+                    if 'an' in placeholder or 'ch' in placeholder:
+                        for tag in chapter_content.find_all(True):
+                            if tag.name in ['script']:
+                                tag.decompose()
 
-        # Save the updated content to a new file
-        output_file_name = f'SM-{version}.html'
-        with open(output_file_name, 'w', encoding='utf-8') as file:
-            file.write(template)
+                    if 'ch' in placeholder:
+                        # Find the <a> tag with id "_Annexes" and modify its id
+                        annex_tag = chapter_content.find('a', id='_Annexes')
+                        if annex_tag:
+                            # Get the third and fourth characters of the placeholder
+                            third_char = placeholder[3]
+                            fourth_char = placeholder[4] if placeholder[4] != '}' else ''
+                            new_id = f'an_{third_char}{fourth_char}'
+                            annex_tag['id'] = new_id
+                        for a in chapter_content.find_all('a', href=True):
+                            href = a['href']
+                            if 'annex' in href:
+                                # Remove everything before the #
+                                href = href[href.index('#'):]
+                                href = '#an' + href[1:]
+                                a['href'] = href
 
-        print(f'Template updated and saved as {output_file_name}')
+                    if 'an' in placeholder:
+                        for a in chapter_content.find_all('a', id=True):
+                            a_id = a['id']
+                            if '_' in a_id:
+                                # Remove everything before the #
+                                a_id = f'an{a_id}'
+                                a['id'] = a_id
+
+                    chapter_content = chapter_content.decode_contents()
+                    toc = str(chapter_content)
+
+                    # Replace the placeholder with the content
+                    template = template.replace(placeholder, toc)
+
+            # Replace {version}, {edate}, and {lang} placeholders
+            template = template.replace('{version}', version)
+            template = template.replace('{edate}', edate)
+            template = template.replace('{lang}', lang)
+
+            # Save the updated content to a new file
+            output_file_name = f'SM-{version}_{lang}.html'
+            with open(output_file_name, 'w', encoding='utf-8') as file:
+                file.write(template)
+
+            print(f'Template updated and saved as {output_file_name}')
     except Exception as e:
         print(f'Error updating template: {e}')
 
---
+++ SMgen_old.py
@@ -0,0 +1,138 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import csv
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+template_file = os.path.join(dir_path, "smtemplate.html")
+csv_file = os.path.join(dir_path, "smurls.csv")
+
+# def prompt_version():
+#     return input('Enter the version (yyyy-m): ')
+# def prompt_edate():
+#     return input('Enter the effective date (yyyy-mm-dd): ')
+# def update_template():
+#     try:
+#         version = prompt_version()
+#         edate = prompt_edate()
+
+def update_template():
+    try:
+        version = "2024-8"
+        edate = "2024-08-06"
+
+        lang = "en"
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        # Read the smtemplate.html file
+        with open(template_file, 'r', encoding='utf-8') as file:
+            template = file.read()
+        # Read the smurls.csv file
+        with open(csv_file, 'r', encoding='utf-8-sig') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                placeholder, url = row
+                response = requests.get(url, headers=headers)
+                response.raise_for_status()
+                # Parse the HTML content
+                soup = BeautifulSoup(response.content, 'html.parser')
+                chapter_content = soup.find('div', id='ChapterContent') or soup.find('div', id='smg-main')
+                if chapter_content is None:
+                    raise ValueError(f'ChapterContent div not found for URL: {url}')
+                # Remove all tags that are not <ul>, <li>, or <a>
+
+                if placeholder == '{toc}':
+                    # Remove the first <h3> tag
+                    first_h3 = chapter_content.find('h3')
+                    if first_h3:
+                        first_h3.decompose()
+                    last_a = chapter_content.find_all('a')[-1]
+                    if last_a:
+                        last_a.decompose()
+                    last_h3 = chapter_content.find_all('h3')[-1]
+                    if last_h3:
+                        a_gl_tag = soup.new_tag('a', href='#gl')
+                        a_gl_tag.string = '1 Supply Manual glossary'
+                        last_h3.replace_with(a_gl_tag)
+                    # Process remaining <h3> tags
+                    for h3 in chapter_content.find_all('h3'):
+                        strong_tag = h3.find('strong')
+                        if strong_tag and len(strong_tag.text) >= 5:
+                            href_second = strong_tag.text[9] if strong_tag.text[9].isdigit() else ''
+                            href_value = strong_tag.text[8]
+                            a_tag = soup.new_tag('a', href=f'#_{href_value}{href_second}')
+                            a_tag.string = f'{href_value}{href_second} {h3.text}'
+                            h3.replace_with(a_tag)
+                    for tag in chapter_content.find_all(True):
+                        if tag.name not in ['section', 'h3', 'strong', 'ul', 'li', 'a']:
+                            tag.decompose()
+                    # Replace all <section> tags with <li> tags
+                    for section in chapter_content.find_all('section'):
+                        li_tag = soup.new_tag('li')
+                        li_tag.extend(section.contents)
+                        section.replace_with(li_tag)
+                    # Modify href attributes in <a> tags within chapter_content
+                    for a in chapter_content.find_all('a', href=True):
+                        href = a['href']
+                        if '#' in href:
+                            # Remove everything before the #
+                            href = href[href.index('#'):]
+                            a['href'] = href
+                        if 'annex' in href:
+                            a_content = a.text
+                            an_chap = a_content[-2:] if len(a_content) == 22 else a_content[-1]
+                            href = f'#an_{an_chap}'
+                            a['href'] = href
+
+                if 'an' in placeholder or 'ch' in placeholder:
+                    for tag in chapter_content.find_all(True):
+                        if tag.name in ['script']:
+                            tag.decompose()
+
+                if 'ch' in placeholder:
+                    # Find the <a> tag with id "_Annexes" and modify its id
+                    annex_tag = chapter_content.find('a', id='_Annexes')
+                    if annex_tag:
+                        # Get the third and fourth characters of the placeholder
+                        third_char = placeholder[3]
+                        fourth_char = placeholder[4] if placeholder[4] != '}' else ''
+                        new_id = f'an_{third_char}{fourth_char}'
+                        annex_tag['id'] = new_id
+                    for a in chapter_content.find_all('a', href=True):
+                        href = a['href']
+                        if 'annex' in href:
+                            # Remove everything before the #
+                            href = href[href.index('#'):]
+                            href = '#an' + href[1:]
+                            a['href'] = href
+
+                if 'an' in placeholder:
+                    for a in chapter_content.find_all('a', id=True):
+                        a_id = a['id']
+                        if '_' in a_id:
+                            # Remove everything before the #
+                            a_id = f'an{a_id}'
+                            a['id'] = a_id
+
+                chapter_content = chapter_content.decode_contents()
+                toc = str(chapter_content)
+
+                # Replace the placeholder with the content
+                template = template.replace(placeholder, toc)
+
+        # Replace {version}, {edate}, and {lang} placeholders
+        template = template.replace('{version}', version)
+        template = template.replace('{edate}', edate)
+        template = template.replace('{lang}', lang)
+
+        # Save the updated content to a new file
+        output_file_name = f'SM-{version}.html'
+        with open(output_file_name, 'w', encoding='utf-8') as file:
+            file.write(template)
+
+        print(f'Template updated and saved as {output_file_name}')
+    except Exception as e:
+        print(f'Error updating template: {e}')
+
+# Run the main function
+if __name__ == '__main__':
+    update_template()
